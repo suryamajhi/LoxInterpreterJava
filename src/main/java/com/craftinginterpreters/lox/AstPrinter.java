f6005738252cbf3aa7904996f329e39e6b9ba878
Surya <suryamajhi20@gmail.com>
2024-09-13 14:21:12 +0545

    Control flow

--- src/main/java/com/craftinginterpreters/lox/AstPrinter.java
+++ src/main/java/com/craftinginterpreters/lox/AstPrinter.java
@@ -5,6 +5,12 @@ public class AstPrinter implements Expr.Visitor<String>{
 	String print(Expr expr) {
 		return expr.accept(this);
 	}
+
+	@Override
+	public String visitAssignExpr(Expr.Assign expr) {
+		return null;
+	}
+
 	@Override
 	public String visitBinaryExpr(Expr.Binary expr) {
 		return parenthesize(expr.operator.lexeme, expr.left, expr.right);
@@ -22,8 +28,18 @@ public class AstPrinter implements Expr.Visitor<String>{
 	}
 
 	@Override
+	public String visitLogicalExpr(Expr.Logical expr) {
+		return null;
+	}
+
+	@Override
 	public String visitUnaryExpr(Expr.Unary expr) {
 		return parenthesize(expr.operator.lexeme, expr.right);
+	}
+
+	@Override
+	public String visitVariableExpr(Expr.Variable expr) {
+		return expr.name.lexeme;
 	}
 
 	private String parenthesize(String name, Expr... exprs) {
--- src/main/java/com/craftinginterpreters/lox/Parser.java
+++ src/main/java/com/craftinginterpreters/lox/Parser.java
@@ -1,5 +1,6 @@
 package com.craftinginterpreters.lox;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.craftinginterpreters.lox.TokenType.*;
@@ -28,16 +29,166 @@ public class Parser {
 		this.tokens = tokens;
 	}
 
-	Expr parse() {
+	List<Stmt> parse() {
+		List<Stmt> statements = new ArrayList<>();
+		while(!isAtEnd()) {
+			statements.add(declaration());
+		}
+		return statements;
+	}
+
+	/**
+	 * expression     → assignment ;
+	 * @return
+	 */
+	private Expr expression() {
+		return assignment();
+	}
+
+	/**
+	 * 	 * assignment     → IDENTIFIER "=" assignment | equality ;
+	 *
+	 * REVISIED:
+	 * assignment     → IDENTIFIER "=" assignment
+	 *                | logic_or ;
+	 * logic_or       → logic_and ( "or" logic_and )* ;
+	 * logic_and      → equality ( "and" equality )* ;
+	 *
+	 * @return
+	 */
+	private Expr assignment() {
+		Expr expr = or();
+		if(match(EQUAL)) {
+			Token equals = previous();
+			Expr value = assignment();
+
+			if(expr instanceof  Expr.Variable) {
+				Token name = ((Expr.Variable) expr).name;
+				return new Expr.Assign(name, value);
+			}
+			error(equals, "Invalid assignment target.");
+		}
+		return expr;
+	}
+
+	/**
+	 * logic_or       → logic_and ( "or" logic_and )* ;
+	 * @return
+	 */
+	private Expr or() {
+		Expr expr = and();
+		while(match(OR)) {
+			Token operator = previous();
+			Expr right = and();
+			expr = new Expr.Logical(expr, operator, right);
+		}
+		return expr;
+	}
+
+	/**
+	 *  logic_and      → equality ( "and" equality )* ;
+	 * @return
+	 */
+	private Expr and() {
+		Expr expr = equality();
+
+		while(match(AND)) {
+			Token operator = previous();
+			Expr right = equality();
+			expr = new Expr.Logical(expr, operator, right);
+		}
+		return expr;
+	}
+
+	private Stmt declaration() {
 		try {
-			return expression();
+			if (match(VAR)) return varDeclaration();
+			return statement();
 		} catch (ParseError error) {
+			synchronize();
 			return null;
 		}
 	}
 
-	private Expr expression() {
-		return equality();
+	private Stmt varDeclaration() {
+		Token name = consume(IDENTIFIER, "Expect variable name");
+
+		Expr initializer = null;
+		if(match(EQUAL)) {
+			initializer = expression();
+		}
+
+		consume(SEMICOLON, "Expect ';' after variable declaration");
+
+		return new Stmt.Var(name, initializer);
+	}
+
+	/**
+	 * statement      → exprStmt
+	 *                | printStmt
+	 *                | ifStmt
+	 *                | whileStmt
+	 *                | block ;
+	 * @return
+	 */
+	private Stmt statement() {
+		if(match(IF)) return ifStatement();
+		if(match(PRINT)) return printStatement();
+		if(match(WHILE)) return whileStatement();
+		if(match(LEFT_BRACE)) return new Stmt.Block(block());
+		return expressionStatement();
+	}
+
+	/**
+	 * whileStmt      → "while" "(" expression ")" statement ;
+	 * @return
+	 */
+	private Stmt whileStatement() {
+		consume(LEFT_PAREN, "Expect '(' after 'while'.");
+		Expr condition = expression();
+		consume(RIGHT_PAREN, "Expect ')' after condition.");
+		Stmt body = statement();
+		return new Stmt.While(condition, body);
+	}
+
+
+	/**
+	 * ifStmt         → "if" "(" expression ")" statement
+	 *                ( "else" statement )? ;
+	 * @return
+	 */
+	private Stmt ifStatement() {
+		consume(LEFT_PAREN, "Expect '(' after 'if'.");
+		Expr condition = expression();
+		consume(RIGHT_PAREN, "Expect ')' after if condition.");
+
+		Stmt thenBranch = statement();
+		Stmt elseBranch = null;
+		if(match(ELSE)) {
+			elseBranch = statement();
+		}
+		return new Stmt.If(condition, thenBranch, elseBranch);
+	}
+
+	private Stmt printStatement() {
+		Expr value = expression();
+		consume(SEMICOLON, "Expect ':' after value.");
+		return new Stmt.Print(value);
+	}
+
+	private Stmt expressionStatement() {
+		Expr expr = expression();
+		consume(SEMICOLON, "Expect ';' after expression.");
+		return new Stmt.Expression(expr);
+	}
+
+	private List<Stmt> block() {
+		List<Stmt> statements = new ArrayList<>();
+		while(!check(RIGHT_BRACE) && !isAtEnd()) {
+			statements.add(declaration());
+		}
+		consume(RIGHT_BRACE, "Expect '}' after block");
+		return statements;
 	}
 
 	/**
@@ -163,6 +314,9 @@ public class Parser {
 
 		if (match(NUMBER, STRING)) {
 			return new Expr.Literal(previous().literal);
+		}
+		if(match(IDENTIFIER)) {
+			return new Expr.Variable(previous());
 		}
 
 		if (match(LEFT_PAREN)) {
