549eb08c7b1ef27b6f480a20ac1873ba292950e0
Surya <suryamajhi20@gmail.com>
2024-09-14 08:28:46 +0545

    Resolving and Binding

--- src/main/java/com/craftinginterpreters/lox/Interpreter.java
+++ src/main/java/com/craftinginterpreters/lox/Interpreter.java
@@ -1,12 +1,15 @@
 package com.craftinginterpreters.lox;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
 	final Environment globals = new Environment();
 	private Environment environment = new Environment();
+	private final Map<Expr, Integer> locals = new HashMap<>();
 
 	Interpreter() {
 		globals.define("clock", new LoxCallable() {
@@ -152,8 +155,18 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
 	@Override
 	public Object visitVariableExpr(Expr.Variable expr) {
-		return environment.get(expr.name);
+		return lookUpVariable(expr.name, expr);
 	}
+
+	private Object lookUpVariable(Token name, Expr expr) {
+		Integer distance = locals.get(expr);
+		if(distance != null) {
+			return environment.getAt(distance, name.lexeme);
+		} else {
+			return globals.get(name);
+		}
+	}
+
 
 	private void checkNumberOperand(Token operator, Object operand) {
 		if(operand instanceof Double) return;
@@ -192,6 +205,10 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
 	private void execute(Stmt stmt) {
 		stmt.accept(this);
+	}
+
+	void resolve(Expr expr, int depth) {
+		locals.put(expr, depth);
 	}
 
 	void executeBlock(List<Stmt> statements, Environment environment) {
@@ -271,7 +288,13 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 	@Override
 	public Object visitAssignExpr(Expr.Assign expr) {
 		Object value = evaluate(expr.value);
-		environment.assign(expr.name, value);
+
+		Integer distance = locals.get(expr);
+		if(distance != null) {
+			environment.assignAt(distance, expr.name, value);
+		} else {
+			globals.assign(expr.name, value);
+		}
 		return value;
 	}
 }
