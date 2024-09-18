2cc48bcb9600580c91f57708dfe6ac3aa101b7bc
Surya <suryamajhi20@gmail.com>
2024-09-18 07:12:55 +0545

    New env set to globals.

--- src/main/java/com/craftinginterpreters/lox/Interpreter.java
+++ src/main/java/com/craftinginterpreters/lox/Interpreter.java
@@ -8,7 +8,7 @@ import java.util.Map;
 public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
 	final Environment globals = new Environment();
-	private Environment environment = new Environment();
+	private Environment environment = globals;
 	private final Map<Expr, Integer> locals = new HashMap<>();
 
 	Interpreter() {
